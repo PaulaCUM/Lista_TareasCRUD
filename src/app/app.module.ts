f79f12bf1a36cfc343bab7bc7e1fd405106c807c
Paula Useche <paula.cum2@gmail.com>
2023-01-27 16:40:36 -0500

    Diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppComponent } from './app.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { ListTasksComponent } from './components/list-tasks/list-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTaskComponent,
+    TasksComponent,
+    ListTasksComponent
   ],
   imports: [
     BrowserModule
